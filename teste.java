cee4998af4201daf5750d54c6bb0cfa7428bcaf4
João Pedro Silva Souza Nunes <126632669+Joao-Pedro12@users.noreply.github.com>
2023-03-29 08:53:39 -0300

    Add files via upload

---
+++ teste.java
@@ -0,0 +1,20 @@
+
+public class teste {
+
+	public static void main(String[] args) {
+		System.out.println("Primeiro projeto em Java");
+		
+		double teste1 = 50;
+		double teste2 = 25;
+		
+		
+		double resultado = teste1 - teste2;
+		
+		
+		System.out.println("O resultado da subtração é: " + resultado);
+		
+		
+
+	}
+
+}
